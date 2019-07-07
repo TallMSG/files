608e33195607ca9e3b96851c2e0343c17503e8af
TallMSG <42735558+TallMSG@users.noreply.github.com>
2019-07-08 00:49:15 +0300

    new file

--- files.py
+++ files.py
@@ -9,56 +9,101 @@ with open('recipes.txt') as f:
         else:
             output.write(line)
 
-with open('dish1.txt') as f:
-    ingridients = []
-    for line in f:
-        line = line.strip()
-        ingridients.append(line)
-    # print(type(ingridients))
-    ing_list = []
-    for item in ingridients:
+def open_dish(a):
+
+    with open(a) as f:
+        ingridients = []
+        for line in f:
+            line = line.strip()
+            ingridients.append(line)
         dishname = str(ingridients[0])
         ing_numb = int(ingridients[1])
-        split_list2 = ingridients[2].split(' | ')
-        split_list3 = ingridients[3].split(' | ')
-        split_list4 = ingridients[4].split(' | ')
-    ing_list.append(split_list2)
-    ing_list.append(split_list3)
-    ing_list.append(split_list4)
-    # print(dishname)
-    # print(ing_list)
-    ing_descript = {}
-    for item in ing_list:
-        ing_descript['ingridient_name'] = item[0]
-        ing_descript['quantity'] = item[1]
-        ing_descript['measure'] = item[2]
-        print(ing_descript)
+        ingridients.remove(ingridients[0])
+        ingridients.remove(ingridients[0])
 
-# Не могу сохранить словарь после цикла for, сохраняются только последние значения
+        ing_list = []
+        for item in ingridients:
+            aaa = item.split(' | ')
+            ing_list.append(aaa)
+        # print(dishname)
+        # print(ing_numb)
+        # print(ing_list)
 
+        ing_descript = {}
+        for item in ing_list:
+            ing_descript[item[0]] = {}
+            ing_descript[item[0]]['ingridient_name'] = item[0]
+            ing_descript[item[0]]['quantity'] = item[1]
+            ing_descript[item[0]]['measure'] = item[2]
+        # print(ing_descript)
 
-
-
-
-
-
-
-    # print(f'Читаем файл после цикла: {f.read()}')
-# print(f'Все строрки: {all_lines}')
+    return ing_descript
 
 
 
-    #   считать первую строку файла:
-    #   for line in range(1):
-    #     head = next(f).strip()
+def open_all ():
+    a = open_dish('dish1.txt')
+    b = open_dish('dish2.txt')
+    c = open_dish('dish3.txt')
+    cook_book = {}
+    cook_book['Омлет'] = a
+    cook_book['Утка по-пекински'] = b
+    cook_book['Запечёный картофель'] = c
+    # print(cook_book)
+    return cook_book
+
+
+# open_all()
 
 
 
 
-# преобразовать строки в список
-# with open(fname) as f:
-#     content = f.readlines()
-# # you may also want to remove whitespace characters like '\n' at the end of each line
-# content = [x.strip() for x in content]
+
+# person_count = int(input('Введите количество персон: '))
+
+
+
+def cook_omelet():
+    uuu = open_all()
+    for k, v in uuu.items():
+        omelet_ing = {}
+        if k == 'Омлет':
+            omelet_ing = v
+
+
+        for k, v in omelet_ing.items():
+            new_dict = omelet_ing[k]
+            # print(new_dict)
+
+
+            for k, v in new_dict.items():
+                name = new_dict['ingridient_name']
+                if k == 'ingridient_name':
+                    sss = del new_dict[k]
+                    print(sss)
+
+
+
+
+cook_omelet()
+
+
+# def get_shop_list_by_dishes(dishes, person_count):
+
+# while True:
+#     print('Введите блюда:')
+#     dishes = input('Омлет - a, Утка по-пекински - b, Запечённый картофель - c: ')
+#     if dishes == 'a':
+#         cook_omelet()
+#     elif dishes == 'b':
+#         cook_duck()
+#     elif dishes == 'c':
+#         cook_potatoes()
+#     else:
+#         print('Такого блюда не существует')
+
+
+
+
 
 
